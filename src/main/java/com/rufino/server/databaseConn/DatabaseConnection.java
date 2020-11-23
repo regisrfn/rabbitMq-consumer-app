c60af5b70d7b6fc142847cee86ea82b3659d1d63
Regis Rufino <regis.rfnrodrigues@gmail.com>
2020-11-23 15:19:24 -0300

    Testing connection to DB

--- src/main/java/com/rufino/server/databaseConn/DatabaseConnection.java
+++ src/main/java/com/rufino/server/databaseConn/DatabaseConnection.java
@@ -4,8 +4,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import org.springframework.beans.factory.annotation.Value;
-
 public class DatabaseConnection {
 
     private static DatabaseConnection instance;
